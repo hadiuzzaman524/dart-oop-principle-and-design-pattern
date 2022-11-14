2ed2d28e2bb430969484d526522209db1aeae6b9
hadi.heavytask <hadi@heavytask.com>
2022-11-14 10:06:49 +0600

    feat: just create a project

---
+++ test/design_patter_with_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:design_patter_with_dart/design_patter_with_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

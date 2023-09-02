ffe65d7bb1f7fdbf3603d30578b19ee2c2dd5514
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2023-09-02 11:29:44 +0600

    chore: learn about the open closed principle

---
+++ lib/SOLID/open_close_principle.dart
@@ -0,0 +1,71 @@
+/*
+Open for extension and closed for modification
+
+Bad Code: 
+class User {
+  final String name;
+  final int age;
+  final String phoneNumber;
+  User({required this.name, required this.age, required this.phoneNumber});
+  @override
+  String toString() {
+    return "Name: $name, Age: $age, Phone: $phoneNumber";
+  }
+}
+
+/// the class is not closed for modification, if we need to save user information
+/// in local storage or on network we need to modify. so its violate the principle
+
+class StoreUser {
+  final User user;
+  StoreUser(this.user);
+  void saveToDB() {
+    print("Save to DB");
+  }
+   void saveToFile() {
+    print("Save to File");
+  }
+}
+
+*/
+
+class User {
+  final String name;
+  final int age;
+  final String phoneNumber;
+  User({required this.name, required this.age, required this.phoneNumber});
+  @override
+  String toString() {
+    return "Name: $name, Age: $age, Phone: $phoneNumber";
+  }
+}
+
+abstract class SaveInformation {
+  void saveUserInformation(final User user);
+}
+
+class LocalStorage implements SaveInformation {
+  @override
+  void saveUserInformation(User user) {
+    print("Saved on Local Storage $user");
+  }
+}
+
+class CloudStorage implements SaveInformation {
+  @override
+  void saveUserInformation(User user) {
+    print("Save on Cloud Storage $user");
+  }
+}
+
+void main() {
+  final user = User(
+    name: "Md Hadiuzzaman",
+    age: 23,
+    phoneNumber: "01908034074",
+  );
+  final localStorage = LocalStorage();
+  localStorage.saveUserInformation(user);
+  final cloudStorage = CloudStorage();
+  cloudStorage.saveUserInformation(user);
+}
